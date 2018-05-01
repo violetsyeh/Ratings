e254f31332408983391afd409936fe44991143f6
Hackbright Student <no-reply@hackbrightacademy.com>
2018-05-01 12:09:07 -0700

    Still working on register form

--- server.py
+++ server.py
@@ -2,10 +2,13 @@
 
 from jinja2 import StrictUndefined
 
-from flask import Flask
 from flask_debugtoolbar import DebugToolbarExtension
 
-from model import connect_to_db, db
+from flask import (Flask, render_template, redirect, request, flash,
+                   session)
+
+from model import User, Rating, Movie, connect_to_db, db
+
 
 
 app = Flask(__name__)
@@ -22,7 +25,41 @@ app.jinja_env.undefined = StrictUndefined
 @app.route('/')
 def index():
     """Homepage."""
-    return "<html><body>Placeholder for the homepage.</body></html>"
+    
+    return render_template("homepage.html")
+
+@app.route("/users")
+def user_list():
+    """Show list of users."""
+
+    users = User.query.all()
+    for user in users:
+        # print user
+        # break
+        return render_template("user_list.html", users=users)
+
+@app.route("/show-registration")
+def show_registration():
+    """Shows registration form"""
+
+    return render_template("register-form.html")
+
+@app.route('/register', methods=['POST'])
+def register_form():
+
+    email = request.args.get("email")
+    password = request.args.get("password")
+
+    users = User.query.all()
+
+    if email in users.email:
+        redirect('/login')
+    else:
+        user = User(email=email, password=password)
+        db.session.add(user)
+        db.session.commit()
+        return redirect('/')
+
 
 
 if __name__ == "__main__":
